bbf4e208b63084ee0fa05c4a06ec852cedf38617
Milan Wikarski <milan@wikarski.sk>
2019-12-05 22:11:27 +0100

    Some changes in comments

--- example/bot.py
+++ example/bot.py
@@ -17,7 +17,7 @@ updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
 
-# Commands
+# Command /start
 def command_start(update, context):
   print("TEST")
   context.bot.send_message(
@@ -26,6 +26,7 @@ def command_start(update, context):
 dispatcher.add_handler(CommandHandler("start", command_start))
 
 
+# Command /help
 def command_help(update, context):
   context.bot.send_message(
       chat_id=update.effective_chat.id, text="Dokážem nájsť text piesne pomocou príkazu /lyrics")
@@ -33,15 +34,17 @@ def command_help(update, context):
 dispatcher.add_handler(CommandHandler("help", command_help))
 
 
-def command_upper(update, context):
+# Command /caps
+def command_caps(update, context):
   text = " ".join(context.args).upper()
 
   context.bot.send_message(
       chat_id=update.effective_chat.id, text=text)
 
-dispatcher.add_handler(CommandHandler("caps", command_upper))
+dispatcher.add_handler(CommandHandler("caps", command_caps))
 
 
+# Command /lyrics
 def command_lyrics(update, context):
   url = "https://some-random-api.ml/lyrics?title="
   title = " ".join(context.args)
@@ -64,6 +67,6 @@ def command_lyrics(update, context):
 dispatcher.add_handler(CommandHandler("lyrics", command_lyrics))
 
 
-# Start
+# Start the bot
 updater.start_polling()
 print("Bot is running...")
